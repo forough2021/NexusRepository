5e48ea25b09cc624898cd17bd720ae747d222b27
forough <forough.safari1986@gmail.com>
2021-08-26 13:02:30 +0430

    first commit

---
+++ NexusProject/src/com/NexusProject/NexusProject.java
@@ -0,0 +1,10 @@
+package com.NexusProject;
+
+public class NexusProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi my friend");
+	}
+
+}
