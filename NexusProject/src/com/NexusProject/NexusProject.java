37a020289bf027eaddc917424cf3eb0c63235d66
forough2021 <81819863+forough2021@users.noreply.github.com>
2021-10-03 08:46:49 +0330

    Update NexusProject.java
    Adding to message

--- NexusProject/src/com/NexusProject/NexusProject.java
+++ NexusProject/src/com/NexusProject/NexusProject.java
@@ -4,7 +4,7 @@ public class NexusProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi my friends");
+System.out.println("hi my dear friends");
 	}
 
 }
